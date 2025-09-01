b10745c5843200d07a4adb50e608f99bb08fff52
sid6394 <sid6394928238@.in>
2025-09-01 21:53:57 +0530

    data type

---
+++ datatype.py
@@ -0,0 +1,28 @@
+# numbers...
+
+a = 12
+b = -12
+c = 0
+d = 4.1
+e = 4/2
+f = 5j
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+
+# Strings...
+
+g = "siddhant"
+h = "123456789"
+print(type(g))
+print(type(h))
+
+# Boolean....
+
+i = True
+j = False
+print(type(i))
+print(type(j))
