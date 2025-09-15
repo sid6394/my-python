7fa587ca791f2b5b1150ab68e807b9a301d78c7e
sid6394 <sid6394928238@.in>
2025-09-16 01:04:37 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,50 @@
+# arithmatic operator
+a = 12
+b =8
+print(a+b)
+print(a-b)
+print(a*b)
+print(a /b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+# assignment operators
+a = 23
+
+# compound assignment operators
+a = 20
+a = a + 20
+a = a + 40
+print(a)
+
+a=20
+a+=20
+print(a)
+
+# comparision operators
+a = 12
+b = 12
+print(a==b)
+print(a!=b)
+print(a<b)
+print(a>b)
+print(a<=b) 
+print(a>=b)   
+
+# logical operators
+# and
+print(12>20 and 123>100 and 32==32 and 45<90)
+# or
+print(12>20 or 123>100 or 32==33 or 45>90)
+# not
+print(not 13==13)
+
+
+
+   
+
+
+
+
+
